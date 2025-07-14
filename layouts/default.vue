f52cbea79029ecda60210961eeab49c3441dc590
itsslava <77506882+itsslava@users.noreply.github.com>
2025-07-14 20:02:06 +0300

    feat(layout): add default layout and side header with icons

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="default-layout">
+		<CustomHeader />
+		<main><slot /></main>
+	</div>
+</template>
+<style lang="css" scoped>
+.default-layout {
+	display: flex;
+	height: 100vh;
+}
+</style>
